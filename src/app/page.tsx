9be18573852a48ce8c1939f68d57928d69be38e1
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-28 18:37:59 -0300

    chore: base project

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
+function HomePage(){
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <DocumentMagnifyingGlassIcon
+        className="h-64 w-64 text-indigo-600/20"
+      />
+
+      <h1 className="text-3xl text-center mt-2 font-bold mb-5">
+        Welcome to the Amazon Web Scraper
+      </h1>
+    </div>
+  );
+}
+
+export default HomePage;
